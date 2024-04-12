532ba60ae01a6c5a3750ed387a756d0b4536f19d
Yan <you@example.com>
2024-04-12 17:10:09 +0800

    第六次提交-支持多结点-但不支持结点之间进行通信

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory, url_for
 from flask_cors import CORS
 
 from wallet import Wallet
